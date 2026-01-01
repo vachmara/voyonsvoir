243200b40865a666fe9a7a735dcecc44dc3fcdac
francois76 <fgognet@gmail.com>
2026-01-01 14:30:34 +0000

    adding contributors on next project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,16 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main>
+        <main>
         <button className={styles.button}>
           Surprise
         </button>
+          <a
+            className={styles.button}
+            href="/contributors"
+          >
+            🎉 Voir les Contributeurs
+          </a>
       </main>
     </div>
   );
